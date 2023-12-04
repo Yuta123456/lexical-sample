c0d9b963abf81fd2b71d796d36601c24f7819a3f
Yuta123456 <yuuta09090530@icloud.com>
2023-12-05 00:05:27 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+function onError(error: Error) {
+  console.error(error);
+}
+
+type InitialConfig = Parameters<typeof LexicalComposer>[0]["initialConfig"];
+
+function App() {
+  const initialConfig: InitialConfig = {
+    namespace: "MyEditor",
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+    </LexicalComposer>
+  );
+}
+
+export default App;
